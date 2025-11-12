6791ad15bf72b042751458b2b53504f00097538f
Giovany Torres <u202311601@upc.edu.pe>
2025-11-12 12:01:41 -0600

    feat(conductores): update ui

---
+++ flota365/lib/core/ui/app_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const primaryBlue  = Color(0xFF6CDAE7);
+  static const primaryWhite = Color(0xFFFFFFFF);
+
+  // Secondary
+  static const secondaryYellow  = Color(0xFFFFC107);
+  static const secondaryGreen1  = Color(0xFF4CAF50);
+  static const secondaryRed1    = Color(0xFFF44336);
+  static const secondaryYellow3 = Color(0xFFF5E3AE);
+
+  // Wireframe
+  static const gray1 = Color(0xFFD9D9D9);
+  static const gray2 = Color(0xFF8E8E8E);
+  static const black = Color(0xFF282828);
+  static const white = Color(0xFFFFFFFF);
+
+  // Text
+  static const textBlack = black;
+  static const textWhite = white;
+}
