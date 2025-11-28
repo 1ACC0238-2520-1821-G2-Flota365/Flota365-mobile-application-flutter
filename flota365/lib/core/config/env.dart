ea10c99a4a4c755f0d9afe550dc8f835acaec1e3
Giovany Torres <u202311601@upc.edu.pe>
2025-11-28 16:41:48 -0500

    feat(driver): update env

--- flota365/lib/core/config/env.dart
+++ flota365/lib/core/config/env.dart
@@ -2,8 +2,8 @@ class Env {
   // Permite cambiar la base por --dart-define
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://underground-tuesday-renworkplace-1e2821cb.koyeb.app',
+    defaultValue: 'https://flota365-backend-corp-cmawf5ddamh5f7b8.westus3-01.azurewebsites.net',
   );
-
+  static const ORS_API_KEY = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjlmNDI0OWE3OTVlZTQwMjdhODg3MzdmYzA0MmIzMjIxIiwiaCI6Im11cm11cjY0In0=';
   static const isProd = bool.fromEnvironment('IS_PROD', defaultValue: false);
 }
